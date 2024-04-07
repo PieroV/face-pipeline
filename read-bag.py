bfd0b8ddb8ba1886ebc533e14422ddc1c6c3077a
Piero V. <vogliadifarniente@gmail.com>
2024-04-07 22:25:55 +0200

    Improved the script to read Realsense bags.

--- read-bag.py
+++ read-bag.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import json
-import os
+from pathlib import Path
 
 import cv2
 import numpy as np
@@ -16,6 +16,25 @@ parser.add_argument(
     type=float,
     help="Threshold on the depth for the combined file",
 )
+parser.add_argument(
+    "-d",
+    "--output-directory",
+    type=Path,
+    help="Destination directory for the data",
+)
+parser.add_argument(
+    "--save", action=argparse.BooleanOptionalAction, default=True
+)
+parser.add_argument(
+    "--save-heatmaps", action=argparse.BooleanOptionalAction, default=False
+)
+parser.add_argument(
+    "--spatial-filter", action=argparse.BooleanOptionalAction, default=True
+)
+parser.add_argument(
+    "--temporal-filter", action=argparse.BooleanOptionalAction, default=True
+)
+
 args = parser.parse_args()
 if not args.input:
     print("No input paramater have been given.")
@@ -23,10 +42,16 @@ if not args.input:
     exit()
 thresh = args.threshold if args.threshold and args.threshold > 0 else 0.0
 
-os.makedirs("rgb", exist_ok=True)
-os.makedirs("depth", exist_ok=True)
-os.makedirs("combined", exist_ok=True)
-os.makedirs("overlapped", exist_ok=True)
+dest = args.output_directory
+if not dest:
+    dest = Path(".")
+if args.save:
+    dest.mkdir(exist_ok=True)
+    (dest / "rgb").mkdir(exist_ok=True)
+    (dest / "depth").mkdir(exist_ok=True)
+    if args.save_heatmaps:
+        (dest / "combined").mkdir(exist_ok=True)
+        (dest / "overlapped").mkdir(exist_ok=True)
 
 # When aligning to depth, invalid depth points will be blacked also in
 # the RGB frame.
@@ -44,19 +69,20 @@ device = profile.get_device()
 playback = device.as_playback()
 playback.set_real_time(False)
 
-vsp = profile.get_stream(align_to).as_video_stream_profile()
-intr = vsp.get_intrinsics()
-intr = {
-    "ppx": intr.ppx,
-    "ppy": intr.ppy,
-    "fx": intr.fx,
-    "fy": intr.fy,
-    "width": vsp.width(),
-    "height": vsp.height(),
-    "scale": device.first_depth_sensor().get_depth_scale(),
-}
-with open("camera.json", "w") as f:
-    json.dump(intr, f)
+if args.save:
+    vsp = profile.get_stream(align_to).as_video_stream_profile()
+    intr = vsp.get_intrinsics()
+    intr = {
+        "ppx": intr.ppx,
+        "ppy": intr.ppy,
+        "fx": intr.fx,
+        "fy": intr.fy,
+        "width": vsp.width(),
+        "height": vsp.height(),
+        "scale": device.first_depth_sensor().get_depth_scale(),
+    }
+    with (dest / "camera.json").open("w") as f:
+        json.dump(intr, f)
 
 align = rs.align(align_to)
 spatial = rs.spatial_filter()
@@ -72,12 +98,17 @@ while True:
     aligned_frames = align.process(frames)
     color_frame = np.asarray(aligned_frames.get_color_frame().get_data())
     color_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(f"rgb/{i}.jpg", color_frame)
     depth = aligned_frames.get_depth_frame()
-    # depth = spatial.process(depth)
-    # depth = temporal.process(depth)
+    if args.spatial_filter:
+        depth = spatial.process(depth)
+    if args.temporal_filter:
+        depth = temporal.process(depth)
     depth_frame = np.asarray(depth.get_data())
-    cv2.imwrite(f"depth/{i}.png", depth_frame)
+
+    if args.save:
+        cv2.imwrite(str(dest / f"rgb/{i}.jpg"), color_frame)
+        cv2.imwrite(str(dest / f"depth/{i}.png"), depth_frame)
+
     trunc = depth_frame.max() / 1000.0 if thresh <= 0.0 else thresh
     depth_m = depth_frame / 1000.0
     depth_m[depth_m > trunc] = 0.0
@@ -85,11 +116,15 @@ while True:
     depth_cm = cv2.applyColorMap(depth8, cv2.COLORMAP_PLASMA)
     adj = cv2.hconcat([color_frame, depth_cm])
     over = (color_frame * 0.5 + depth_cm * 0.5).astype(np.uint8)
-    cv2.imwrite(f"combined/{i}.png", adj)
-    cv2.imwrite(f"overlapped/{i}.png", over)
     cv2.imshow("Combined", adj)
     cv2.imshow("Overlapped", over)
-    cv2.waitKey(1)
+
+    if args.save and args.save_heatmaps:
+        cv2.imwrite(str(dest / f"combined/{i}.png"), adj)
+        cv2.imwrite(str(dest / f"overlapped/{i}.png"), over)
+
+    if cv2.waitKey(1) == 27:
+        break
     print(
         i,
         playback.get_position() * 1e-9,
